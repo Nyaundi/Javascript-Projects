0583f7f88f4d7cf7a8487af1fa832d47b4c9cc41
--unset <jamesmabuka260@gmail.com>
2023-09-28 20:04:07 +0300

    sanitized

--- script.js
+++ script.js
@@ -3,5 +3,13 @@ const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
 btn.addEventListener('click', function() {
-    
-})
+    ///Get a random Number between 0 and 3
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber (){
+    return Math.floor(Math.random() * 4);
+}
