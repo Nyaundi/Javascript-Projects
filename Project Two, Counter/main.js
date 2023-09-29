4e8b270982e370f5514d53008d0aa2ba41e962ce
--unset <jamesmabuka260@gmail.com>
2023-09-29 09:08:54 +0300

    added js

---
+++ Project Two, Counter/main.js
@@ -0,0 +1,12 @@
+//Set initial count
+let count = 0;
+
+//select value and buttons
+const value = document.getElementById('value');
+const btns = document.querySelectorAll('.btn')
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function(e){
+        console.log(e.currentTarget)
+    })
+});
