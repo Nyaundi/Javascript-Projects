b1c0df0def50907300ea50c869cc0fe1ee584f36
--unset <jamesmabuka260@gmail.com>
2023-09-29 09:20:21 +0300

    done

--- Project Two, Counter/main.js
+++ Project Two, Counter/main.js
@@ -7,6 +7,19 @@ const btns = document.querySelectorAll('.btn')
 
 btns.forEach(function (btn) {
     btn.addEventListener('click', function(e){
-        console.log(e.currentTarget)
-    })
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')){
+            count --;
+        }
+        else if(styles.contains('increase')){
+            count ++;
+        }
+        else{
+            count = 0;
+        }
+        if (count > 0) {
+            value.style.color = 'green';
+        }
+        value.textContent = count;
+    });
 });
